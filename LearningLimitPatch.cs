2993a20d3256b5fa2ca1dba5cb344c6e359b9882
CanExiOne <canexione@gmail.com>
2023-09-09 15:09:19 +0200

    Add project files.

---
+++ LearningLimitPatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using System;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.GameComponents;
+using TaleWorlds.Localization;
+
+public class LearningLimitPatch
+{
+    [HarmonyPatch(typeof(DefaultCharacterDevelopmentModel))]
+    [HarmonyPatch("CalculateLearningRate")]
+    [HarmonyPatch(new Type[] { typeof(int), typeof(int), typeof(int), typeof(int), typeof(TextObject), typeof(bool) })]
+    private static void Postfix(ref ExplainedNumber __result)
+    {
+        __result.LimitMin(1f);
+    }
+}
---
+++ SubModule.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+
+namespace MinimumExperienceGain
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            Harmony harmony = new Harmony("minimum_experience_gain");
+
+            harmony.PatchAll();
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+
+        }
+    }
+}
