0e2ca02456d4ed37660a8ce605a094b755a21b08
CanExiOne <canexione@gmail.com>
2023-09-12 13:41:51 +0200

    Change Learning Rate to 1 if skill below 300

--- LearningLimitPatch.cs
+++ LearningLimitPatch.cs
@@ -1,16 +1,51 @@
 ﻿using HarmonyLib;
 using System;
+using System.Reflection;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.GameComponents;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
 using TaleWorlds.Localization;
 
-public class LearningLimitPatch
+namespace MinimumExperienceGain
 {
-    [HarmonyPatch(typeof(DefaultCharacterDevelopmentModel))]
-    [HarmonyPatch("CalculateLearningRate")]
+    [HarmonyPatch(typeof(DefaultCharacterDevelopmentModel), "CalculateLearningRate")]
     [HarmonyPatch(new Type[] { typeof(int), typeof(int), typeof(int), typeof(int), typeof(TextObject), typeof(bool) })]
-    private static void Postfix(ref ExplainedNumber __result)
+    public class LearningLimitPatch
     {
-        __result.LimitMin(1f);
+        static bool Prefix(DefaultCharacterDevelopmentModel __instance, ref ExplainedNumber __result,
+           int attributeValue, int focusValue, int skillValue, int characterLevel, TextObject attributeName, bool includeDescriptions = false)
+        {
+            TextObject _skillFocusText = Traverse.Create<DefaultCharacterDevelopmentModel>().Field("_skillFocusText").GetValue<TextObject>();
+            TextObject _overLimitText = Traverse.Create<DefaultCharacterDevelopmentModel>().Field("_overLimitText").GetValue<TextObject>();
+
+            ExplainedNumber result = new ExplainedNumber(1.25f, includeDescriptions);
+            result.AddFactor(0.4f * (float)attributeValue, attributeName);
+            result.AddFactor((float)focusValue * 1f, _skillFocusText);
+
+            var CalculateLearningLimit = AccessTools.Method(typeof(DefaultCharacterDevelopmentModel), "CalculateLearningLimit",
+                new Type[] { typeof(int), typeof(int), typeof(TextObject), typeof(bool) });
+
+            ExplainedNumber learningLimit = (ExplainedNumber)CalculateLearningLimit.Invoke(__instance, new object[] { attributeValue, focusValue, null, null });
+            int num = TaleWorlds.Library.MathF.Round(learningLimit.ResultNumber);
+            if (skillValue > num)
+            {
+                int num2 = skillValue - num;
+                result.AddFactor(-1f - 0.1f * (float)num2, _overLimitText);
+            }
+            if (skillValue >= 300)
+            {
+                result.LimitMin(0f);
+            } else
+            {
+                result.LimitMin(1f);
+            }
+
+            __result = result;
+
+            return false;
+
+        }
     }
+
 }
